cfda41e9e710474b3fb9884fbb291503965d04d7
avaneeshdevkota <avaneeshdevkota86@gmail.com>
2023-07-30 14:10:51 +0545

    Additional classes for different types of error.

--- pjinx/Error.py
+++ pjinx/Error.py
@@ -6,8 +6,23 @@ class JinxException(BaseException):
         self.line = line
         self.message = message
     
+    def __str__(self):
+
+        print(f"[Line {self.line}] Error : {self.message}")
+    
+
+class JinxSyntaxError(JinxException):
+
+    ...
 
 class JinxParseError(JinxException):
+
+    def __init__(self, token : Token, message : str):
+
+        self.line = token.line
+        self.message = message
+
+class JinxRuntimeError(JinxException):
 
     def __init__(self, token : Token, message : str):
 
