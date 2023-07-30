21d48dcc7e5a7c79e289e08b0037f0da4a8248a5
avaneeshdevkota <avaneeshdevkota86@gmail.com>
2023-07-30 11:15:16 +0545

    Visualize parsing.

---
+++ pjinx/AstPrinter.py
@@ -0,0 +1,45 @@
+from typing import Any
+from TokenType import TokenType
+from Token import Token as T
+from Expr import *
+
+class AstPrinter(ExprVisitor):
+
+    def main(self):
+        pass
+
+    def visit_Binary_Expr(self, expr: Binary):
+
+        return self.parenthesize(expr.operator.lexeme, expr.left, expr.right)
+
+    def visit_Grouping_Expr(self, expr: Grouping):
+
+        return self.parenthesize("group", expr.expr)
+
+    def visit_Literal_Expr(self, expr: Literal):
+
+        if (expr.value == None):
+            return "nil"
+        return str(expr.value)
+
+    def visit_Unary_Expr(self, expr: Unary):
+
+        return self.parenthesize(expr.operator.lexeme, expr.right)
+    
+
+    def print(self, expr: "Expr") -> str:
+
+        return expr.accept(self)
+
+    def parenthesize(self, name, *args):
+
+        s = f"( {name}"
+
+        for arg in args:
+            s += " " + arg.accept(self)
+
+        s += ")"
+        return s
+
+printer = AstPrinter()
+printer.main()
